1d5127c059c8e265c6fab24a907ae393fffe07cf
Theis <theis@mejnertsen.com>
2023-06-21 22:14:30 +0200

    improved create symlink script

--- scripts/create-sym-links.sh
+++ scripts/create-sym-links.sh
@@ -3,9 +3,31 @@
 #get the cloned directory path
 DIR=$(git rev-parse --show-toplevel)
 
+ORIG_FILES=(
+	"$DIR"/.zshrc
+	"$DIR"/.gitconfig
+	"$DIR"/.vimrc
+	"$DIR"/vscode-settings.json
+)
+
+END_FILES=(
+	"$HOME"/.zshrc	
+	"$HOME"/.gitconfig
+	"$HOME"/.vimrc
+	"$HOME"/.config/Code/User/settings.json
+)
+
+INDEX=0
+
+cd "$DIR"
+
 printf "Creating symbolic links, from %s...\n" "$DIR"
-ln -s "$DIR"/.zshrc		~/.zshrc
-ln -s "$DIR"/.gitconfig	~/.gitconfig
-ln -s "$DIR"/.vimrc		~/.vimrc
-#check if file exists first
-ln -s "$DIR"/vscode-settings.json ~/.config/Code/User/settings.json
+for FILE in ${END_FILES[@]}; do
+	printf "%s\n" "$FILE"
+	if [[ -f "$FILE" ]]; then
+		rm "$FILE"
+	fi
+	ln -s ${ORIG_FILES[INDEX]}	"$FILE"
+	((INDEX++))
+done
+printf "...Done\n"
