659ab59ba0c37d6ac3454400fcfac01f57d8be9e
Theis <theis@mejnertsen.com>
2023-06-21 21:34:41 +0200

    added vscode settings to symlink script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,3 +7,5 @@ printf "Creating symbolic links, from %s...\n" "$DIR"
 ln -s "$DIR"/.zshrc		~/.zshrc
 ln -s "$DIR"/.gitconfig	~/.gitconfig
 ln -s "$DIR"/.vimrc		~/.vimrc
+#check if file exists first
+ln -s "$DIR"/vscode-settings.json ~/.config/Code/User/settings.json
