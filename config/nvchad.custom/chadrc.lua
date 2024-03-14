09bf75517489c80e65d29625721bb67616526dc6
TheisM <theismejnertsen@gmail.com>
2024-03-14 22:24:14 +0100

    updated theme

--- config/nvchad.custom/chadrc.lua
+++ config/nvchad.custom/chadrc.lua
@@ -1,7 +1,7 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'bearded-arc' }
+M.ui = { theme = 'nightowl' }
 M.ui.transparency = true
 M.mappings = require "custom.mappings"
 M.plugins = "custom.plugins"
