ac02a005072c9d308645ff8c83267956cd1c269f
TheisM <theismejnertsen@gmail.com>
2024-03-14 22:24:07 +0100

    updated keybindings for git commands

--- config/nvchad.custom/mappings.lua
+++ config/nvchad.custom/mappings.lua
@@ -9,17 +9,28 @@ M.tmux = {
     }
 }
 
-M.general = {
-  -- Insert mode mappings
-  i = {
-    ["jj"] = { "<ESC>", "escape insert mode" , opts = { nowait = true }},
-    ["jk"] = { "<ESC>", "escape insert mode" , opts = { nowait = true }},
-  },
-
-  -- Visual mode mappings 
-  v = {
-    [";;"] = { "<ESC>", "escape visual mode" , opts = { nowait = true }},
+M.git = {
+  n = {
+    ["<leader>gp"] = 
+        {"<cmd> Gitsigns preview_hunk<CR>", "git: preview change"},
+    ["<leader>gb"] = 
+        {"<cmd> Gitsigns toggle_current_line_blame<CR>", "git: toggle line blame"},
+    ["<leader>gd"] = 
+        {"<cmd> Gitsigns diffthis<CR>", "git: open diff"}
   }
+}
+ 
+M.general = {
+    -- Insert mode mappings
+    i = {
+        ["jj"] = { "<ESC>", "escape insert mode" , opts = { nowait = true }},
+        ["jk"] = { "<ESC>", "escape insert mode" , opts = { nowait = true }},
+    },
+
+    -- Visual mode mappings 
+    v = {
+        [";"] = { "<ESC>", "escape visual mode" , opts = { nowait = true }},
+    }
 }
 
 return M
