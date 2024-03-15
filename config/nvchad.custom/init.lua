9d60f0bc299842741a1a791de273266d4efcd26a
Theis <theis@mejnertsen.com>
2024-03-15 08:36:11 +0100

    added segment to start nvimtree on startup when there is no file

--- config/nvchad.custom/init.lua
+++ config/nvchad.custom/init.lua
@@ -5,3 +5,19 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+
+-- Focus NvimTree on open
+vim.api.nvim_create_autocmd(
+    "VimEnter",
+    {
+        callback = function (args)
+
+            local file = vim.api.nvim_buf_get_name(args.buf)
+
+            if file == "" then
+                vim.cmd("NvimTreeFocus")
+            end
+        end
+    }
+)
+
