1b2133ce60734fd9fbe25be39d5ea237a243cd94
Theis <theismejnertsen@gmail.com>
2025-06-01 01:25:01 +0200

    nvim updated set options

--- config/nvim/lua/omegarelay/opt.lua
+++ config/nvim/lua/omegarelay/opt.lua
@@ -1,14 +1,24 @@
+vim.g.have_nerd_font = true
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.smartindent= true
+
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10 -- 10 line margin when scrolling
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
+vim.opt.signcolumn = 'yes'
 vim.opt.colorcolumn = "80,100,120"
 
 vim.opt.clipboard = "unnamedplus"
