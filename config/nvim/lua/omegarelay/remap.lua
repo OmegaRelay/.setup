16eb5db6f85c80e46aa093c1a8c0e8d33e5a771f
Theis <theismejnertsen@gmail.com>
2025-06-01 11:01:25 +0200

    neovim added comments with remappings

--- config/nvim/lua/omegarelay/remap.lua
+++ config/nvim/lua/omegarelay/remap.lua
@@ -4,12 +4,15 @@ vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jj", "<esc>")
 
+-- Search and replace
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+-- Move blocks of text up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
+-- Keep cursor centered when jumping up and down
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
