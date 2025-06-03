079db62e21c90a439f3dfab2317e6bc9bf535c8c
Theis <theismejnertsen@gmail.com>
2025-06-03 22:36:27 +0200

    nvim: added git blame plugin

---
+++ config/nvim/lua/omegarelay/lazy_plugins/git_blame.lua
@@ -0,0 +1,10 @@
+return {
+    "f-person/git-blame.nvim",
+    event = "VeryLazy",
+    opts = {
+        enabled = true,
+        message_template = " <author> • <date> • <<sha>>",
+        date_format = "%S:%M:%H %d-%m-%Y ",
+        virtual_text_column = 80,
+    },
+}
