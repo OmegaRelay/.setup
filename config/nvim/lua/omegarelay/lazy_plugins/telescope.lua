14936ed6a231f5cc6b6100e9d70c4cfe8e3af4ea
Theis Mejnertsen <theis.mejnertsen@sphinx-electronics.de>
2025-06-07 00:36:14 +0200

    nvim set telescope bindings to start with leader f and updated descriptions

--- config/nvim/lua/omegarelay/lazy_plugins/telescope.lua
+++ config/nvim/lua/omegarelay/lazy_plugins/telescope.lua
@@ -1,14 +1,24 @@
-
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     dependencies = { {'nvim-lua/plenary.nvim'} },
     config = function()
+        local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { desc = 'Telescope: ' .. desc })
+        end
+
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Find [F]iles" })
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("grep > ") });
-        end, { desc = "Find [S]tring in Workspace" })
+        map('<leader>ff', builtin.find_files, "[F]ind [F]iles")
+        map('<leader>fg', builtin.git_files, "[F]ind [G]it files")
+        map('<leader>fa', function()
+            builtin.find_files({ no_ignore = true })
+        end, "[F]ind [A]ll Files")
+        map('<leader>fs', function()
+            builtin.grep_string({
+                no_ignore = true,
+                search = vim.fn.input("grep > "),
+            });
+        end, "Find [S]tring in Workspace")
     end,
 }
