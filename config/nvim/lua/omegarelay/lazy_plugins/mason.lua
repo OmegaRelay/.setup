d90ea6574012a5535328d10a74d6414781bdd56b
Theis Mejnertsen <theis.mejnertsen@sphinx-electronics.de>
2025-07-10 19:47:38 +0200

    nvim: updated mason source and added icons
    Signed-off-by: Theis Mejnertsen <theis.mejnertsen@sphinx-electronics.de>

--- config/nvim/lua/omegarelay/lazy_plugins/mason.lua
+++ config/nvim/lua/omegarelay/lazy_plugins/mason.lua
@@ -1,7 +1,15 @@
 return {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     config = function()
-        require("mason").setup()
+        require("mason").setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                }
+            }
+        })
         vim.keymap.set("n", "<leader>m", vim.cmd.Mason, { desc = "Open [M]ason" })
     end
 }
